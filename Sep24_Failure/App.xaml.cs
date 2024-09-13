ee7dea346838ed21a4cbf08b0164e9fe99713db5
hmishra <hmishra@microsoft.com>
2024-09-13 16:18:14 +0530

    Sep24 ReadOnlySpan Optimization Related Failure

---
+++ Sep24_Failure/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sep24_Failure
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
